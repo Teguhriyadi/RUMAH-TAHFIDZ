a3ed6b881bea35c8efe992f92f798846b99317d0
Teguhriyadi <ilham.teguh55@gmail.com>
2022-02-13 17:34:03 +0700

    membuat template

---
+++ APILaravel/app/Http/Controllers/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    public function layouts()
+    {
+        return view("/app/administrator/layouts/template");
+    }
+
+    public function home()
+    {
+        return view("/app/administrator/v_home");   
+    }
+}
---
+++ APILaravel/resources/views/app/administrator/layouts/template.blade.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Rumah Tahfidz | @yield("app_title")</title>
+
+    @include("app/administrator/layouts/partials/css/style")
+</head>
+
+<body>
+    <div id="app">
+        <div class="main-wrapper">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <form class="form-inline mr-auto">
+                    <ul class="navbar-nav mr-3">
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+                    </ul>
+                </form>
+                <ul class="navbar-nav navbar-right">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                        <img alt="image" src="{{ url('/template') }}/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="dropdown-title">Logged in 5 min ago</div>
+                            <a href="features-profile.html" class="dropdown-item has-icon">
+                                <i class="far fa-user"></i> Profile
+                            </a>
+                            <a href="features-activities.html" class="dropdown-item has-icon">
+                                <i class="fas fa-bolt"></i> Activities
+                            </a>
+                            <a href="features-settings.html" class="dropdown-item has-icon">
+                                <i class="fas fa-cog"></i> Settings
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item has-icon text-danger">
+                                <i class="fas fa-sign-out-alt"></i> Logout
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+
+            @include("app/administrator/layouts/partials/sidebar/main-sidebar")
+
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield("app_content")
+            </div>
+            <!-- END Main Content -->
+            @include("app/administrator/layouts/partials/footer/main-footer")
+
+        </div>
+    </div>
+
+    @include("app/administrator/layouts/partials/js/style")
+
+</body>
+</html>
