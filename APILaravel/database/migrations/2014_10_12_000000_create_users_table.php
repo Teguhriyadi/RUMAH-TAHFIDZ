4d0268981898a4af4566652f51ac64794c18acc0
Teguhriyadi <ilham.teguh55@gmail.com>
2022-02-13 17:43:40 +0700

    migrasi users

--- APILaravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ APILaravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("nama", 100);
+            $table->varchar("username", 50);
+            $table->string("email", 100)->unique();
+            $table->string("password", 100);
+            $table->integer("id_role");
             $table->rememberToken();
             $table->timestamps();
         });
