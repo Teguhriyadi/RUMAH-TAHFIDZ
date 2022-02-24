b09a8c4cc497a972f3a477a505911e8144711f12
monarosa22 <monarosanah66@gmail.com>
2022-02-25 00:25:11 +0700

    WEB: Membereskan Sidebar Home

--- APILaravel/resources/views/app/administrator/layouts/partials/sidebar/main-sidebar.blade.php
+++ APILaravel/resources/views/app/administrator/layouts/partials/sidebar/main-sidebar.blade.php
@@ -8,12 +8,11 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class="active"><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                    <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                </ul>
+            <li class="active">
+                <a class="nav-link" href="{{ url('/app/admin/siswa') }}">
+                    <i class="fa fa-home"></i>
+                    <span>Home</span>
+                </a>
             </li>
             <li class="menu-header">Starter</li>
             <li class="nav-item dropdown">
