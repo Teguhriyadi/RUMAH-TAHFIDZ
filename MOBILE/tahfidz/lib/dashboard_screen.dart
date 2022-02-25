ecdab7f984beb78452bcf23cec0f85cff04d601d
= <lelymariakova@gmail.com>
2022-02-25 14:54:40 +0700

    Creating avatar section

--- MOBILE/tahfidz/lib/dashboard_screen.dart
+++ MOBILE/tahfidz/lib/dashboard_screen.dart
@@ -13,6 +13,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
     final heightBody = MediaQuery.of(context).size.height;
     final widhtBody = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: SafeArea(
         child: Stack(
           alignment: Alignment.center,
@@ -22,7 +23,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
               height: 252,
               width: widhtBody,
               child: Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     color: Color.fromARGB(255, 24, 0, 238),
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(50),
@@ -31,16 +32,45 @@ class _DashboardScreenState extends State<DashboardScreen> {
             ),
             Positioned(
               top: 130,
-              width: 350,
-              height: 250,
+              width: widhtBody / 1.25,
+              height: heightBody / 2.5,
               child: Card(
                 color: Colors.white,
                 shape: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(35),
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                      
-                    )),
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(children: const [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.green,
+                      child: CircleAvatar(
+                        radius: 45,
+                        backgroundImage: AssetImage('assets/images/avatar.png'),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "Nandang Eka Prasetya",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Pengajar",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]),
+                ),
               ),
             ),
           ],
