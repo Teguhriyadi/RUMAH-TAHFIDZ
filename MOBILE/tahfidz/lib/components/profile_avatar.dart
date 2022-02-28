71609a01b74d8a4cd1902ce109b6466dde8ed217
naneps <ekaprasetya2244@gmail.com>
2022-02-28 19:35:43 +0700

    MOBILE : Create Profile Screen

--- MOBILE/tahfidz/lib/components/profile_avatar.dart
+++ MOBILE/tahfidz/lib/components/profile_avatar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tahfidz/pages/pengajar/profile/profile_screen.dart';
 
 class ProfilePicture extends StatelessWidget {
   // const ProfilePicture({Key? key}) : super(key: key);
@@ -22,31 +23,33 @@ class ProfilePicture extends StatelessWidget {
             backgroundImage: AssetImage("assets/images/avatar.png"),
           ),
           Positioned(
-      right: 3,
-      bottom: 3,
-      child: SizedBox(
-        height: sizeBtn,
-        width: sizeBtn,
-        child: Center(
-          child: TextButton(
-            style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-                side: BorderSide(color: Colors.white),
+            right: 3,
+            bottom: 3,
+            child: SizedBox(
+              height: sizeBtn,
+              width: sizeBtn,
+              child: Center(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      side: BorderSide(color: Colors.white),
+                    ),
+                    // primary: Colors.white,
+                    backgroundColor: Color.fromARGB(255, 15, 75, 255),
+                  ),
+                  onPressed: () {
+                    Get.to(ProfileScreen());
+                  },
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: sizeIcon,
+                  ),
+                ),
               ),
-              // primary: Colors.white,
-              backgroundColor: Color.fromARGB(255, 15, 75, 255),
-            ),
-            onPressed: () {Get.to(page)},
-            child: Icon(
-              Icons.edit,
-              color: Colors.white,
-              size: sizeIcon,
             ),
           ),
-        ),
-      ),
-    ),
         ],
       ),
     );
--- MOBILE/tahfidz/lib/pages/pengajar/profile/profile_screen.dart
+++ MOBILE/tahfidz/lib/pages/pengajar/profile/profile_screen.dart
@@ -1,9 +1,19 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tahfidz/components/profile_avatar.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool showPassword = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +22,125 @@ class ProfileScreen extends StatelessWidget {
       right: true,
       top: true,
       bottom: true,
-      child: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
+      child: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Text(
+                "Edit Profile",
+                style: GoogleFonts.poppins(
+                    fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Stack(
+                  children: [
+                    Card(
+                      shape: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(199),
+                          borderSide: BorderSide(color: Colors.transparent)),
+                      elevation: 10,
+                      color: Colors.white,
+                      child: ProfilePicture(
+                        sizeAvatar: 130,
+                        sizeBtn: 30,
+                        sizeIcon: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              buildTextField(
+                  "Nama Lengkap", "Nandang Eka Prasetya", false, false),
+              buildTextField(
+                  "Nama Lengkap", "Nandang Eka Prasetya", false, false),
+              buildTextField(
+                  "Nama Lengkap", "Nandang Eka Prasetya", false, false),
+              buildTextField(
+                  "Nama Lengkap", "Nandang Eka Prasetya", false, false),
+              buildTextField(
+                  "Nomor Telepon", "Nandang Eka Prasetya", false, true),
+              buildTextField("Password", "********", true, false),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlineButton(
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () {},
+                    child: Text("CANCEL",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.black)),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "SAVE",
+                      style: TextStyle(
+                          fontSize: 14,
+                          letterSpacing: 2.2,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     ));
+  }
+
+  Widget buildTextField(String labelText, String placeholder,
+      bool isPasswordTextField, bool type) {
+    final typekey = TextInputType.number;
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        obscureText: isPasswordTextField ? showPassword : false,
+        // ignore: dead_code
+        keyboardType: (type == true) ? typekey : null,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.grey,
+                    ),
+                  )
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
+      ),
+    );
   }
 }
